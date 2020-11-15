a75c6cdfcbe5e327b50b544df56732de555dc479
ARSADavis <adavis@appliedres.com>
2020-11-15 05:36:28 -0500

    Added wstring padding functions.

--- code/Globals.h
+++ code/Globals.h
@@ -15,12 +15,13 @@
 #include <codecvt>
 #include <exception>
 #include <functional>
+#include <cmath>
 
 #include "ByteGlobals.h"
 
-#define TreeKeyType size_t
+#define TreeKeyType intptr_t
 
-static std::wstring StringToWString(std::string convertMe)
+static std::wstring StringToWString(std::string const& convertMe)
 {
     return std::wstring(convertMe.begin(), convertMe.end());
 }
@@ -33,4 +34,44 @@ static std::string TimeToString(time_t* convertMe)
 static std::wstring TimeToWString(time_t* convertMe)
 {
     return std::to_wstring(*convertMe);
+}
+
+static std::wstring PadTheLeft(std::wstring const& padMe, wchar_t const& withMe, std::intptr_t desiredLength)
+{
+    std::intptr_t lengthDifference = desiredLength - padMe.length();
+    if (lengthDifference > 0)
+    {
+        return std::wstring(lengthDifference, withMe) + padMe;
+    }
+    else
+    {
+        return padMe;
+    }
+}
+
+static std::wstring PadTheRight(std::wstring const& padMe, wchar_t const& withMe, std::intptr_t desiredLength)
+{
+    std::intptr_t lengthDifference = desiredLength - padMe.length();
+    if (lengthDifference > 0)
+    {
+        return padMe + std::wstring(lengthDifference, withMe);
+    }
+    else
+    {
+        return padMe;
+    }
+}
+
+static std::wstring PadCentered(std::wstring const& padMe, wchar_t const& withMe, std::intptr_t desiredLength)
+{
+    std::intptr_t lengthDifference = desiredLength - padMe.length();
+    if (lengthDifference > 0)
+    {
+        return std::wstring((size_t)ceil((float)lengthDifference / 2.0), withMe) + padMe +
+            std::wstring((size_t)floor((float)lengthDifference / 2.0), withMe);
+    }
+    else
+    {
+        return padMe;
+    }
 }
